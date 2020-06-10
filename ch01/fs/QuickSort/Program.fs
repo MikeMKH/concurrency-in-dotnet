242e9241f6d7204f67eab9d592361aacde079912
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-10 06:35:43 -0500

    add quicksort with depth example

--- ch01/fs/QuickSort/Program.fs
+++ ch01/fs/QuickSort/Program.fs
@@ -3,12 +3,31 @@ module Program
 open System
 open System.Threading.Tasks
 
+type ParallelismHelpers =
+    static member MaxDepth =
+        int (Math.Log(float Environment.ProcessorCount, 2.0))
+
 let rec quicksort aList =
     match aList with
     | [] -> []
     | x :: xs ->
         let lt, gt =
             List.partition (fun n -> n <= x) xs
-        let left  = Task.Run(fun () -> quicksort lt) // #A
-        let right = Task.Run(fun () -> quicksort gt)  // #A
-        left.Result @ (x :: right.Result)              // #B
+        let left  = Task.Run(fun () -> quicksort lt)
+        let right = Task.Run(fun () -> quicksort gt)
+        left.Result @ (x :: right.Result)
+
+let rec quicksortWithDepth depth aList =
+    match aList with
+    | [] -> []
+    | x :: xs ->
+        let lt, gt =
+            List.partition (fun n -> n <= x) xs
+        if depth < 0 then
+          let left  = lt |> quicksortWithDepth depth
+          let right = gt |> quicksortWithDepth depth
+          left @ (x :: right) 
+        else
+          let left  = Task.Run(fun () -> lt |> quicksortWithDepth (depth - 1))
+          let right = Task.Run(fun () -> gt |> quicksortWithDepth (depth - 1))
+          left.Result @ (x :: right.Result)
--- ch01/fs/QuickSort/Tests.fs
+++ ch01/fs/QuickSort/Tests.fs
@@ -1,8 +1,6 @@
 module Tests =
 
-  open System
   open Program
-  open FsCheck
   open FsCheck.Xunit
   
   [<Property>]
@@ -12,3 +10,18 @@ module Tests =
   [<Property>]
   let ``should be in order`` (xs: int list) =
       xs |> quicksort |> Seq.pairwise |> Seq.forall(fun (x, y) -> x <= y)
+  
+  [<Property>]
+  let ``should be idempotent`` (xs: int list) =
+      (xs |> quicksort) = (xs |> quicksort |> quicksort)
+  
+  [<Property>]
+  let ``should have same result`` (xs: int list) =
+      (xs |> quicksort) = (xs |> quicksortWithDepth 3)
+  
+  [<Property>]
+  let ``should be in order with max depth`` (xs: int list) =
+      xs
+        |> quicksortWithDepth ParallelismHelpers.MaxDepth
+        |> Seq.pairwise |> Seq.forall(fun (x, y) -> x <= y)
+  
