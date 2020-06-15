6913f2669840df270b81010f3e9da53c91928abe
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-15 06:31:34 -0500

    add examples ch2.6 - 2.7

--- ch02/fs/Examples/Program.fs
+++ ch02/fs/Examples/Program.fs
@@ -1,8 +1,19 @@
 module Program
 
+open System.Collections.Generic
 open System.Collections.Concurrent
+open System.Linq
+open FSharp.Collections.ParallelSeq
 
 module Memoization =
-    let memoize (f: 'a -> 'b) =
-        let m = ConcurrentDictionary<'a,'b>()
-        fun x ->   m.GetOrAdd(x, f)
+  let memoize (f: 'a -> 'b) =
+    let m = ConcurrentDictionary<'a,'b>()
+    fun x ->   m.GetOrAdd(x, f)
+
+module ConsurrentSpeculation =
+  let fuzzyMatch (numbers: int list) =
+  let numbersSet = new HashSet<int>(numbers)
+  fun number ->
+      numbersSet
+        |> PSeq.sortBy(fun n -> abs (number - n))
+        |> Seq.head
--- ch02/fs/Examples/Tests.fs
+++ ch02/fs/Examples/Tests.fs
@@ -1,6 +1,7 @@
 module Tests
 
 open Program.Memoization
+open Program.ConsurrentSpeculation
 open Xunit
 open FsCheck.Xunit
 
@@ -32,4 +33,31 @@ let ``memoize functions are only called once for same argument`` () =
   
   f 1 |> ignore
   Assert.Equal(2, counter)
-  
+
+[<Fact>]
+let ``fuzzy match will find exact match at beginning`` () =
+  let numberMatch = fuzzyMatch [1; 2; 3]
+  let expected = 1
+  let actual = numberMatch expected
+  Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``fuzzy match will find exact match at end`` () =
+  let numberMatch = fuzzyMatch [1; 2; 3]
+  let expected = 3
+  let actual = numberMatch expected
+  Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``fuzzy match will find nearest matches below value`` () =
+  let numberMatch = fuzzyMatch [3; 7; 30; 4; 100]
+  let expected = 4
+  let actual = numberMatch (expected + 1)
+  Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``fuzzy match will find nearest matches above value`` () =
+  let numberMatch = fuzzyMatch [3; 7; 30; 6; 100]
+  let expected = 6
+  let actual = numberMatch (expected - 1)
+  Assert.Equal(expected, actual)
