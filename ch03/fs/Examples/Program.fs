902079114f3b7bd588aababf3dc85991f966d04c
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-18 06:38:52 -0500

    add examples ch 3.3

--- ch03/fs/Examples/Program.fs
+++ ch03/fs/Examples/Program.fs
@@ -1,1 +1,18 @@
-module Program = let [<EntryPoint>] main _ = 0
+module Program
+
+module FunctionalDataStructure =
+  type FList<'a> =
+    | Empty
+    | Cons of head: 'a * tail: FList<'a>
+    
+  let rec map f list: FList<'a> =
+    match list with
+    | Empty -> Empty
+    | Cons(hd, tl) -> Cons(f hd, map f tl)
+    
+  let rec fold f (m: 'b) (list: FList<'a>) =
+    match list with
+    | Empty -> m
+    | Cons(hd, tl) -> fold f (f m hd) tl
+    
+  let length list = fold (fun m _ -> m + 1) 0 list
--- ch03/fs/Examples/Tests.fs
+++ ch03/fs/Examples/Tests.fs
@@ -1,8 +1,25 @@
 module Tests
 
 open System
+open Program.FunctionalDataStructure
 open Xunit
+open FsCheck.Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<Property>]
+let ``map identity FList gives equal FList`` (list: FList<int>) =
+    map id list = list
+    
+[<Property>]
+let ``map is composible`` (list: FList<int>) =
+  map (fun x -> x * 2) list |> map (fun x -> x * 3) = map (fun x -> x * 2 * 3) list
+  
+[<Property>]
+let ``folding two numbers with addition is the same as adding them`` (a: int, b: int) =
+  let expected = a + b
+  let list = Cons(a, Cons(b, Empty))
+  fold (+) 0 list = expected
+  
+[<Property>]
+let ``length of FList is same after map`` (list: FList<string>) =
+  let uppercase (x: string) = if x <> null then x.ToUpper() else null
+  map uppercase list |> length = (list |> length)
