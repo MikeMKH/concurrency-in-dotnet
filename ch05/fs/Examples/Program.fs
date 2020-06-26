d534a6d42c5b7a4bfb47a76df1e097aa0448657e
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-26 06:33:23 -0500

    add example ch 5.2.3 - 5.2.4

--- ch05/fs/Examples/Program.fs
+++ ch05/fs/Examples/Program.fs
@@ -1,4 +1,5 @@
 module Program
+  open System
   open FSharp.Collections.ParallelSeq
   
   let wordPartitoner (lines: string list) =
@@ -11,3 +12,18 @@ module Program
       
   let map' (f: 'a -> 'b) col: 'a list =
     Seq.fold (fun m x -> m @ [f x]) [] col
+    
+  
+  let isPrime n =
+    if n < 2 then false
+    elif n = 2 then true
+    else
+      let boundary = int(Math.Floor(Math.Sqrt(float(n))))
+      [2..boundary]
+        |> Seq.exists(fun x -> n % x = 0)
+        |> not
+    
+  let primeSum boundary =
+    [|0..boundary|]
+      |> Array.Parallel.choose (fun x -> if isPrime x then Some x else None)
+      |> Array.sum
--- ch05/fs/Examples/Tests.fs
+++ ch05/fs/Examples/Tests.fs
@@ -2,6 +2,7 @@ module Tests
 
 open System
 open Xunit
+open FsCheck
 open FsCheck.Xunit
 open Program
 
@@ -32,3 +33,18 @@ let ``fold map inc [0; 1] => [1; 2]`` () =
 [<Property>]
 let ``fold map gives same results as map`` (col: int list, f: int -> int) =
   map' f col = List.map f col
+
+[<Theory>]
+[<InlineData(2, 2)>]
+[<InlineData(3, 5)>]
+[<InlineData(4, 5)>]
+[<InlineData(5, 10)>]
+[<InlineData(6, 10)>]
+[<InlineData(100, 1060)>]
+let ``prime sum gives expected total`` (boundary: int, expected: int) =
+  Assert.Equal(expected, primeSum boundary)
+  
+[<Property>]
+let ``prime sum gives same value as doing it by hand`` (n: PositiveInt) =
+  ((int) n |> primeSum) =  
+    (Seq.init ((int) n) (fun x -> x + 1) |> Seq.filter isPrime |> Seq.sum)
