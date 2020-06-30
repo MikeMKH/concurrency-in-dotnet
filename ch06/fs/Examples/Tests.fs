dfc454fa12f72906e18a3e15d0fdd2ca09d0f520
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-30 06:27:58 -0500

    add example ch 6.1 - 6.2

--- ch06/fs/Examples/Tests.fs
+++ ch06/fs/Examples/Tests.fs
@@ -1,8 +1,42 @@
 module Tests
 
 open System
+open System.Threading
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``example of timer`` () = 
+  let printer _ = printf "timer:   ms=%A\n" DateTime.Now.Millisecond
+  let timer = new Timers.Timer(100.0) // ms
+  
+  timer.AutoReset <- true
+  timer.Elapsed.Add printer
+  
+  let t = async {
+      timer.Start()
+      do! Async.Sleep 1000 // ms
+      timer.Stop
+  }
+  Async.RunSynchronously t
+  
+  Assert.True(true)
+  
+[<Fact>]
+let ``example of counter`` () =
+  let mutable count = 0
+  let counter _ =
+    count <- count + 1
+    printf "counter: count=%i\n" count
+  let timer = new Timers.Timer(100.0) // ms
+  
+  timer.AutoReset <- true
+  timer.Elapsed.Add counter
+  
+  let t = async {
+      timer.Start()
+      do! Async.Sleep 1000 // ms
+      timer.Stop
+  }
+  Async.RunSynchronously t
+  
+  Assert.True(true)
