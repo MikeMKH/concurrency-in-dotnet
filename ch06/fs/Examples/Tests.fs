6e5437ddd2170e5677e5877b62a2d8c826f16a7b
Mike Harris <MikeMKH@users.noreply.github.com>
2020-07-01 06:33:12 -0500

    add example ch 6.3

--- ch06/fs/Examples/Tests.fs
+++ ch06/fs/Examples/Tests.fs
@@ -2,10 +2,11 @@ module Tests
 
 open System
 open System.Threading
+open FSharp.Control.Reactive
 open Xunit
 
 [<Fact>]
-let ``example of timer`` () = 
+let ``example of Event timer`` () = 
   let printer _ = printf "timer:   ms=%A\n" DateTime.Now.Millisecond
   let timer = new Timers.Timer(100.0) // ms
   
@@ -22,7 +23,7 @@ let ``example of timer`` () =
   Assert.True(true)
   
 [<Fact>]
-let ``example of counter`` () =
+let ``example of Event counter`` () =
   let mutable count = 0
   let counter _ =
     count <- count + 1
@@ -40,3 +41,31 @@ let ``example of counter`` () =
   Async.RunSynchronously t
   
   Assert.True(true)
+
+let createTimer interval totalTime =
+  let timer = new Timers.Timer(float interval)
+  (
+    async {
+      timer.Start()
+      do! Async.Sleep(totalTime)
+      timer.Stop()
+    },
+    timer.Elapsed
+  )
+
+[<Fact>]
+let ``example of Observable counter`` () =
+  let mutable count = 0
+  let inc _ =
+    count <- count + 1
+    count
+    
+  let task, source = createTimer 10 100
+  let counter = source |> Observable.map inc
+  
+  counter
+    |> Observable.subscribe (fun n -> printf "Observable: count=%i\n" n)
+    |> ignore
+  
+  Async.RunSynchronously task
+  
