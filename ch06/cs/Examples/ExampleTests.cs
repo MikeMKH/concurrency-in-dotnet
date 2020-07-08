4b3434d52a5df275b57bb4477768d2f52ec10e1e
Mike Harris <MikeMKH@users.noreply.github.com>
2020-07-08 06:35:25 -0500

    add example ch 6.5

--- ch06/cs/Examples/ExampleTests.cs
+++ ch06/cs/Examples/ExampleTests.cs
@@ -2,6 +2,9 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LanguageExt;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Subjects;
 using Xunit;
 
 namespace Examples
@@ -9,12 +12,45 @@ namespace Examples
     public class ExampleTests
     {
         [Fact]
-        public async void MonadicBindingTask()
+        public async void MonadicBindingTaskAsync()
         {
             Task<int> result = from task in Task.Run<int>(() => 40) select task + 2;
             
             int value = await result;
             Assert.Equal(42, value);
+        }
+        
+        [Fact]
+        public void AsyncSubjectPublishesOnlyLastValue()
+        {
+            ISubject<int> subject = new AsyncSubject<int>();
+            int value = 0;
+            
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnCompleted();
+            subject.Subscribe(x => value = x);
+            
+            Assert.Equal(2, value);
+        }
+        
+        [Fact]
+        public void AsyncSubjectPublishesOnlyLastValueWhenYouSubscribeDoesNotMatter()
+        {
+            ISubject<int> subject = new AsyncSubject<int>();
+            int r1 = 0;
+            int r2 = 0;
+            
+            subject.Subscribe(x => r1 = x);
+            
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnCompleted();
+            
+            subject.Subscribe(x => r2 = x);
+            
+            Assert.Equal(2, r1);
+            Assert.Equal(r1, r2);
         }
     }
 }
