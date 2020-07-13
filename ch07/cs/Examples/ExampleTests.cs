8cd4479d5db4969a089fb78c81a75e1ac8ec0e0a
Mike Harris <MikeMKH@users.noreply.github.com>
2020-07-13 06:21:43 -0500

    add examples ch 7.3

--- ch07/cs/Examples/ExampleTests.cs
+++ ch07/cs/Examples/ExampleTests.cs
@@ -130,6 +130,67 @@ namespace Examples
             Assert.Equal(7, x1);
             Assert.Equal(8, x2);
         }
- 
+        
+        [Fact]
+        public void TestUnitEqualsUnit()
+        {
+            var x = Unit.Default;
+            var y = Unit.Default;
+            
+            Assert.True(x.Equals(y));
+            Assert.True(y.Equals(x));
+            Assert.True(x == y);
+            Assert.True(y == x);
+            Assert.Equal(x, y);
+        }
+        
+        [Fact]
+        public void TestUnitDoesNotEqualAnythingElse()
+        {
+            string s = "";
+            int i = default(int);
+            Unit u = Unit.Default;
+            
+            Assert.False(u.Equals(s));
+            Assert.False(s.Equals(u));
+            
+            Assert.False(u.Equals(i));
+            Assert.False(i.Equals(u));
+        }
+        
+        TResult Compute<TInput, TResult>(Task<TInput> task, Func<TInput, TResult> projection)
+          => projection(task.Result);
+        
+        [Fact]
+        public void TestComputeWithBooleanResult()
+        {
+            Task<int> t1 = Task.Run(() => 42);
+            Task<int> t2 = Task.Run(() => 8);
+         
+            Func<int, bool> is42 = n => n == 42;
+            Assert.True(Compute(t1, is42));
+            Assert.False(Compute(t2, is42));
+        }
+        
+        [Fact]
+        public void TestComputeWithUnitResult()
+        {
+            Task<int> t1 = Task.Run(() => 42);
+            Task<int> t2 = Task.Run(() => 8);
+         
+            bool was42 = false;
+            Func<int, Unit> printAndStore = n =>
+            {
+                Console.WriteLine($"Unit: Does {n} == 42? {n == 42}");
+                was42 = n == 42;
+                return Unit.Default;
+            };
+            
+            Compute(t1, printAndStore);
+            Assert.True(was42);
+            
+            Compute(t2, printAndStore);
+            Assert.False(was42);
+        }
     }
 }
---
+++ ch07/cs/Examples/Unit.cs
@@ -0,0 +1,12 @@
+using System;
+
+public struct Unit : IEquatable<Unit>
+{
+  public static readonly Unit Default = new Unit();
+  public override int GetHashCode() => 0;
+  public override bool Equals(object obj) => obj is Unit;
+  public override string ToString() => "()";
+  public bool Equals(Unit other) => true;
+  public static bool operator ==(Unit lhs, Unit rhs) => true;
+  public static bool operator !=(Unit lhs, Unit rhs) => false;
+}
