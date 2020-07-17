cad9185827945757023f4ccb40df6a6293b4111e
Mike Harris <MikeMKH@users.noreply.github.com>
2020-07-17 06:24:49 -0500

    add examples ch 8.2

--- ch08/fs/Examples/Tests.fs
+++ ch08/fs/Examples/Tests.fs
@@ -1,8 +1,25 @@
 module Tests
 
 open System
+open System.IO
+open System.Net
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Example of parallel asynchronous computations`` () =
+  let httpAsync (url: string) = async {
+    let req = WebRequest.Create(Uri(url))
+    let! res = req.AsyncGetResponse()
+    use stream = res.GetResponseStream()
+    use reader = new StreamReader(stream)
+    let! text = reader.ReadToEndAsync() |> Async.AwaitTask
+    return text
+  }
+  
+  let sites = [ "http://www.google.com"; "http://www.amazon.com" ]
+  
+  sites
+    |> Seq.map httpAsync
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> fun text -> Assert.True(text.Length > 0)
