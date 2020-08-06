ece3c2c511e49b4469c600d91b6ea37cd694edf0
Mike Harris <MikeMKH@users.noreply.github.com>
2020-08-06 06:26:16 -0500

    add examples ch 10.1

--- ch10/fs/Examples/Tests.fs
+++ ch10/fs/Examples/Tests.fs
@@ -3,6 +3,36 @@ module Tests
 open System
 open Xunit
 
+// based on https://fsharpforfunandprofit.com/posts/exceptions/
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``System.Exception example`` () =
+  let problem = fun () -> failwith "99 problems" |> ignore
+  Assert.Throws<System.Exception>(problem)
+
+[<Fact>]
+let ``ArgumentException example`` () =
+  let problem = fun () -> invalidArg "argument" "not valid and stuff" |> ignore
+  Assert.Throws<ArgumentException>(problem)
+
+[<Fact>]
+let ``NullArgumentException example`` () =
+  let problem = fun () -> invalidArg "argument" "is null and stuff" |> ignore
+  Assert.Throws<ArgumentException>(problem)
+
+[<Fact>]
+let ``InvalidOperationException example`` () =
+  let problem = fun () -> invalidOp "invalid operation and stuff" |> ignore
+  Assert.Throws<InvalidOperationException>(problem)
+
+[<Fact>]
+let ``FieldAccessException C# sytle example`` () =
+  let problem = fun () -> raise (FieldAccessException "your code's bad and you should feel bad") |> ignore
+  Assert.Throws<FieldAccessException>(problem)
+
+exception SomethingBadHappen of int
+
+[<Fact>]
+let ``custom exception example`` () =
+  let problem = fun () -> raise (SomethingBadHappen 77) |> ignore
+  Assert.Throws<SomethingBadHappen>(problem)
