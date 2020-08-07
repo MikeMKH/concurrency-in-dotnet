8f39552ceac35eafa807a39657cd7a3746546515
Mike Harris <MikeMKH@users.noreply.github.com>
2020-08-07 06:30:04 -0500

    add examples ch 10.2.1 - 10.2.3

--- ch10/fs/Examples/Program.fs
+++ ch10/fs/Examples/Program.fs
@@ -1,1 +1,9 @@
-module Program = let [<EntryPoint>] main _ = 0
+module Program
+  type AsyncOption<'T> = Async<Option<'T>>
+  
+  let complexFunction(value: int): AsyncOption<int> =
+    async {
+      match (value % 2) with
+      | 0 -> return Some(value)
+      | 1 -> return None
+    }
--- ch10/fs/Examples/Tests.fs
+++ ch10/fs/Examples/Tests.fs
@@ -2,6 +2,7 @@ module Tests
 
 open System
 open Xunit
+open Program
 
 // based on https://fsharpforfunandprofit.com/posts/exceptions/
 
@@ -36,3 +37,21 @@ exception SomethingBadHappen of int
 let ``custom exception example`` () =
   let problem = fun () -> raise (SomethingBadHappen 77) |> ignore
   Assert.Throws<SomethingBadHappen>(problem)
+  
+[<Theory>]
+[<InlineData(2)>]
+[<InlineData(4)>]
+[<InlineData(44)>]
+[<InlineData(4400)>] // rock solid testing
+let ``given an even value complexFunction returns value`` (even: int) =
+  let result = complexFunction(even) |> Async.RunSynchronously
+  Assert.Equal(Some(even), result)
+  
+[<Theory>]
+[<InlineData(1)>]
+[<InlineData(11)>]
+[<InlineData(13)>]
+[<InlineData(1321)>] // again rock solid testing
+let ``given an odd value complexFunction returns None`` (odd: int) =
+  let result = complexFunction(odd) |> Async.RunSynchronously
+  Assert.Equal(None, result)
