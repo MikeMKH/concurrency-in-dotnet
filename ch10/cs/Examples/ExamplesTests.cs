9a99982c10df718e860d2ab3c1c0b2c200adb7d9
Mike Harris <MikeMKH@users.noreply.github.com>
2020-08-14 06:00:45 -0500

    add LanguageExt

--- ch10/cs/Examples/ExamplesTests.cs
+++ ch10/cs/Examples/ExamplesTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
 using Xunit;
 
 namespace Examples
