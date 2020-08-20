f94a49b0d717f8db6c8aca68eb765737c48d1f8b
Mike Harris <MikeMKH@users.noreply.github.com>
2020-08-20 06:32:46 -0500

    add example from ch 11.4.1

--- ch11/fs/Examples/Tests.fs
+++ ch11/fs/Examples/Tests.fs
@@ -1,8 +1,22 @@
 module Tests
 
 open System
+open System.Threading
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Mailbox Processor example`` () =
+  let mutable size = 0
+  let agent = MailboxProcessor<string>.Start(fun inbox -> async {
+    while true do
+      let! message = inbox.Receive()
+      size <- message.Length
+      printfn "MailboxProcessor: %s content-length=%d" message message.Length
+  })
+  
+  let message = "hello"
+  agent.Post message
+  while size = 0 do Thread.Sleep(10) // dangerous, but that is how I roll (not really but this is a kata)
+  Assert.Equal(message.Length, size)
+  
+  
