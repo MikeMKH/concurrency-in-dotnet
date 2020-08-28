1018d57d5b8ff9cb9ad7164400e7ce3d31320d54
Mike Harris <MikeMKH@users.noreply.github.com>
2020-08-28 06:34:45 -0500

    add examples 12.2.1

--- ch12/cs/Examples/ExamplesTests.cs
+++ ch12/cs/Examples/ExamplesTests.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using Xunit;
 
 namespace Examples
@@ -6,9 +11,27 @@ namespace Examples
     public class ExamplesTests
     {
         [Fact]
-        public void Test1()
+        public async void ExampleBufferBlockAsync()
         {
-
+            BufferBlock<int> buffer = new BufferBlock<int>();
+            
+            IEnumerable<int> values = Enumerable.Range(1, 100);
+            await Task.WhenAll(
+                ProducerAsync(values),
+                ConsumerAsync(x => Assert.True(x > 0)));
+            
+            async Task ProducerAsync(IEnumerable<int> values)
+            {
+                foreach(var value in values)
+                  buffer.Post(value);
+                buffer.Complete();
+            }
+            
+            async Task ConsumerAsync(Action<int> process)
+            {
+                while(await buffer.OutputAvailableAsync())
+                  process(await buffer.ReceiveAsync());
+            }
         }
     }
 }
