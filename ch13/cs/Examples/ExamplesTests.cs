fcda1cd862afb98d8034de32891488879b080d84
Mike Harris <MikeMKH@users.noreply.github.com>
2020-09-10 06:16:19 -0500

    add examples ch 13.1

--- ch13/cs/Examples/ExamplesTests.cs
+++ ch13/cs/Examples/ExamplesTests.cs
@@ -1,14 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.ObjectPool;
 using Xunit;
 
 namespace Examples
 {
     public class ExamplesTests
     {
-        [Fact]
-        public void Test1()
+        public class Hello
         {
-
+            public string Value { get => "hello"; }
+        }
+        
+        [Fact]
+        public void ExampleObjectPool()
+        {
+            var policy = new DefaultPooledObjectPolicy<Hello>();
+            var pool = new DefaultObjectPool<Hello>(policy);
+            var hello = pool.Get();
+            var value = hello.Value;
+            pool.Return(hello);
+            Assert.Equal("hello", hello.Value);
         }
     }
 }
