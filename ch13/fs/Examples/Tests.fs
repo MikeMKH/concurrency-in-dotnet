b7c122bc0df28a7fdebfd1427980fcb0934debaa
Mike Harris <MikeMKH@users.noreply.github.com>
2020-09-18 06:31:15 -0500

    add I and Maybe example

--- ch13/fs/Examples/Tests.fs
+++ ch13/fs/Examples/Tests.fs
@@ -18,13 +18,35 @@ let (>=>) (f: 'a->Async<'b>) (g: 'b->Async<'c>) (x: 'a) =
   kleisli f g x
 
 [<Fact>]
-let ``given lifted function we can compose with kleisli`` () =
-  let kasync x = fun y -> async { return x }
-  
-  let f = kasync "Hello"
-  let g = kasync 8
+let ``given lifted function we can compose K with kleisli`` () =
+  let k x = fun y -> async { return x }
+  let f = k "Hello"
+  let g = k 8
   let h = f >=> g
   
   let actual = h 9.0 |> Async.RunSynchronously
   
   Assert.Equal(8, actual)
+
+[<Fact>]
+let ``given lifted function we can compose I with kleisli`` () =
+  let i x = async { return x }
+  let h = i >=> i
+  
+  let actual = h "Hello" |> Async.RunSynchronously
+  
+  Assert.Equal("Hello", actual)
+
+[<Fact>]
+let ``given lifted function we can compose option with kleisli`` () =
+  let f x = async { return Some x }
+  let g x = async {
+    return match x with
+           | Some _ -> "got it"
+           | None -> "nothing" 
+  }
+  let h = f >=> g
+  
+  let actual = h "Hello" |> Async.RunSynchronously
+  
+  Assert.Equal("got it", actual)
